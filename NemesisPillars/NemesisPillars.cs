a3a1cd876b96a7c708edf390bc21f293c6da015b
icebro <potatotot98@gmail.com>
2024-12-06 01:08:13 -0700

    literally everything

---
+++ NemesisPillars/NemesisPillars.cs
@@ -0,0 +1,40 @@
+using BepInEx;
+using BepInEx.Configuration;
+using RiskOfOptions;
+using RiskOfOptions.OptionConfigs;
+using RiskOfOptions.Options;
+
+namespace NemesisPillars
+{
+    [BepInDependency("com.rune580.riskofoptions")]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    
+    public class NemesisPillars : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "icebro";
+        public const string PluginName = "nemesispillars";
+        public const string PluginVersion = "1.0.0";
+        
+        public void Awake()
+        {
+            ConfigEntry<float> pillarConfig = Config.Bind<float>("color",
+                "pillars",
+                2,
+                "how many pillars required ,, note going over 4 might softlock you if you cant pillar skip ,,..,,.,");
+
+            SliderConfig slider = new SliderConfig();
+            slider.max = 6;
+            slider.min = 1;
+            slider.FormatString = "{0:0}";
+            
+           ModSettingsManager.AddOption(new SliderOption(pillarConfig, slider));
+            On.RoR2.MoonBatteryMissionController.Awake += (orig, self) =>
+            {
+                orig(self);
+                self._numRequiredBatteries = (int)pillarConfig.Value;
+            };
+        }
+        
+    }
+}
---
+++ Thunderstore/README.md
@@ -0,0 +1,16 @@
+# makes the ready button red
+just evil like that ,..,<br>
+yeah .....,<br>
+whatre yuo gonna do about it .,.,.,,.,., <br>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+
+
+
+thats what i thought .,.,.,.,.,.,.,,,.,. <br>
+
+
+![me goingh yeahhh !! woooo!!!!!!!!!! yippiieiie  !!!woooooooo!!! yeahhhh  !! ](https://media1.tenor.com/m/yG-n8BTJk0kAAAAd/konata-konata-izumi.gif "this is me rn ,.. (not you because i said so .,.,.")
